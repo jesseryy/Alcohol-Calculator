d3cd5c4230e491f4f013ce8cb179024e0a5fd31b
jesseryy <jesse.ryynala@gmail.com>
2022-09-16 21:07:34 +0300

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+ 
+ const [weight, setWeight] = useState(0)
+ const [bottles, setBottles] = useState(0)
+ const [time, setTime] = useState(0)
+ const [gender, setGender] = useState("male")
+ const [alcohol, setAlcohol] = useState(0)
+ 
+
+ const calculate = (e) => {
+  e.preventDefault()
+  let result = 0
+  let litres = bottles * 0.33
+  let grams = litres * 8 * 4.5
+  let burning = weight / 10
+  let gleft = grams - (burning * time)
+
+  if (gender === "male"){
+    result = gleft / (weight * 0.7)
+  } else {
+    result = grams / (weight * 0.6)
+  }
+  setAlcohol(result)
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div id="container">
+  <h3>Calculating alcohol blood level</h3>
+  <form onSubmit={calculate}>
+    <div>
+      <label>Weight</label>
+      <input value={weight} onChange={e => setWeight(e.target.value)}  />
     </div>
+    <div>
+      <label>Bottles</label>
+      <input type="number" value={bottles} onChange={e => setBottles(e.target.value)}/>
+    </div>
+    <div>
+      <label>Time</label>
+      <input type="number" value={time} onChange={e => setTime(e.target.value)}/>
+    </div>
+    <div>
+      <label>Gender</label>
+      <input type="radio" name="gender" value="Male" onChange={e => setGender(e.target.value)} defaultChecked/><label>Male</label>
+      <input type="radio" name="gender" value="Female" onChange={e=> setGender(e.target.value)}/><label>Female</label>
+    </div>
+    <div>
+      <output>{alcohol.toFixed(2)}</output>
+    </div>
+  <button>Calculate</button>
+  </form>
+ </div>
   );
 }
 
